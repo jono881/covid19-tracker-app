a228f2c1f014c7e20f7d9e67c62b7c9aba0eaa00
Jono <jkwok881@gmail.com>
2021-12-16 18:29:32 +1100

    more edits

--- app/src/main/java/au/edu/unsw/infs3634/covid19tracker/CountryDatabase.java
+++
@@ -1,9 +0,0 @@
-package au.edu.unsw.infs3634.covid19tracker;
-
-import androidx.room.Database;
-import androidx.room.RoomDatabase;
-
-@Database(entities = {Country.class}, version = 1)
-public abstract class CountryDatabase extends RoomDatabase {
-    public abstract CountryDao countryDao();
-}
--- app/src/main/java/au/edu/unsw/infs3634/covid19tracker/Premium.java
+++
@@ -1,7 +0,0 @@
-package au.edu.unsw.infs3634.covid19tracker;
-
-
-public class Premium {
-
-
-}
